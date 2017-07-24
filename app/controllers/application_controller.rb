52dad5ec77948305e9e1b3ccb91bc22ca09a722a
Michael Macphee <macphee@cs.helsinki.fi>
2017-07-24 16:27:20 +0300

    Add "Access-Control-Allow-Origin: *" header to everything

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  before_action :set_allow_origin_header
+
+  private
+    def set_allow_origin_header
+      response.set_header('Access-Control-Allow-Origin', '*');
+    end
 end
