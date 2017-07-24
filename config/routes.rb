5c42e6c242af96e29fc1b87957ac97953d447183
Enzine <sonja.makipaja@kolumbus.fi>
2017-07-24 15:24:58 +0300

    Add one test route to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :points
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/api/v8/courses/:course_id/users/:user_id/points", to: 'points#index'
 end
