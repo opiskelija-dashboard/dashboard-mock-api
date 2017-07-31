df9206e2adf3fe8af7caa123a245e4d757835f95
Juuha <juha.ritakoski@hotmail.com>
2017-07-31 15:02:15 +0300

    Fix thing

--- app/serializers/submission_serializer.rb
+++ app/serializers/submission_serializer.rb
@@ -1,3 +1,8 @@
 class SubmissionSerializer < ActiveModel::Serializer
-  attributes :submission
+  attributes :submission_id, :user_id, :pretest_error, :created_at, :updated_at,
+        :exercise_name, :course_id, :processed, :all_tests_passed, :points, :processing_tried_at,
+        :processing_began_at, :processing_completed_at, :times_sent_to_sandbox, 
+        :processing_attempts_started_at, :params_json, :requires_review, :requests_review,
+        :reviewed, :message_for_reviewer, :newer_submission_reviewed, :review_dismissed,
+        :paste_available, :message_for_paste, :paste_key
 end
