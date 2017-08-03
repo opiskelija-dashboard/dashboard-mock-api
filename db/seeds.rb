34094ef4119105898d6f553f6d19642942468c20
Juuha <juha.ritakoski@hotmail.com>
2017-08-03 12:41:34 +0300

    Add a lot more test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -75,4 +75,166 @@ Submission.create(submission_id: 6, user_id: 15653, created_at: "2017-07-24T10:2
   reviewed: false, message_for_reviewer: "", newer_submission_reviewed: false, review_dismissed: false,
   paste_available: false, message_for_paste: "")
 
-Point.create(exercise_id: 2, point_id: 8, course_id: 1, user_id: 1, submission_id: 6, name: "08-08.5345")  
+Point.create(exercise_id: 2, point_id: 8, course_id: 1, user_id: 1, submission_id: 6, name: "08-08.5345") 
+
+Submission.create(submission_id: 7, user_id: 15653, created_at: "2017-07-28T10:29:01.400+03:00",
+  exercise_name: "osa999", course_id: "214", processed: true, 
+  all_tests_passed: true, points: "08-08.5345", processing_tried_at: "2017-09-20T10:29:01.437+03:00",
+  processing_began_at: "2017-07-20T10:29:02.754+03:00", processing_completed_at: "2017-07-20T10:29:17.364+03:00",
+  times_sent_to_sandbox: 1, processing_attempts_started_at: "2017-09-20T10:29:01.400+03:00", 
+  params_json: "{\"error_msg_locale\":\"fi\"}", requires_review: false, requests_review: false,
+  reviewed: false, message_for_reviewer: "", newer_submission_reviewed: false, review_dismissed: false,
+  paste_available: false, message_for_paste: "")
+
+Point.create(exercise_id: 2, point_id: 9, course_id: 1, user_id: 1, submission_id: 7, name: "08-07")
+
+Submission.create(submission_id: 8, user_id: 15653, created_at: "2017-07-29T10:29:01.400+03:00",
+  exercise_name: "osa999", course_id: "214", processed: true, 
+  all_tests_passed: true, points: "08-08.5345", processing_tried_at: "2017-09-20T10:29:01.437+03:00",
+  processing_began_at: "2017-07-20T10:29:02.754+03:00", processing_completed_at: "2017-07-20T10:29:17.364+03:00",
+  times_sent_to_sandbox: 1, processing_attempts_started_at: "2017-09-20T10:29:01.400+03:00", 
+  params_json: "{\"error_msg_locale\":\"fi\"}", requires_review: false, requests_review: false,
+  reviewed: false, message_for_reviewer: "", newer_submission_reviewed: false, review_dismissed: false,
+  paste_available: false, message_for_paste: "")
+
+Point.create(exercise_id: 3, point_id: 10, course_id: 1, user_id: 1, submission_id: 8, name: "08-08-08")
+
+Submission.create(submission_id: 9, user_id: 15653, created_at: "2017-08-01T10:29:01.400+03:00",
+  exercise_name: "osa999", course_id: "214", processed: true, 
+  all_tests_passed: true, points: "08-08.5345", processing_tried_at: "2017-09-20T10:29:01.437+03:00",
+  processing_began_at: "2017-07-20T10:29:02.754+03:00", processing_completed_at: "2017-07-20T10:29:17.364+03:00",
+  times_sent_to_sandbox: 1, processing_attempts_started_at: "2017-09-20T10:29:01.400+03:00", 
+  params_json: "{\"error_msg_locale\":\"fi\"}", requires_review: false, requests_review: false,
+  reviewed: false, message_for_reviewer: "", newer_submission_reviewed: false, review_dismissed: false,
+  paste_available: false, message_for_paste: "")
+
+Point.create(exercise_id: 2, point_id: 11, course_id: 1, user_id: 1, submission_id: 9, name: "08-09")
+
+Submission.create(submission_id: 10, user_id: 15653, created_at: "2017-08-01T10:29:01.400+03:00",
+  exercise_name: "osa999", course_id: "214", processed: true, 
+  all_tests_passed: true, points: "08-08.5345", processing_tried_at: "2017-09-20T10:29:01.437+03:00",
+  processing_began_at: "2017-07-20T10:29:02.754+03:00", processing_completed_at: "2017-07-20T10:29:17.364+03:00",
+  times_sent_to_sandbox: 1, processing_attempts_started_at: "2017-09-20T10:29:01.400+03:00", 
+  params_json: "{\"error_msg_locale\":\"fi\"}", requires_review: false, requests_review: false,
+  reviewed: false, message_for_reviewer: "", newer_submission_reviewed: false, review_dismissed: false,
+  paste_available: false, message_for_paste: "")
+
+Point.create(exercise_id: 2, point_id: 12, course_id: 1, user_id: 1, submission_id: 10, name: "08-10")
+
+Submission.create(submission_id: 11, user_id: 15653, created_at: "2017-08-01T10:29:01.400+03:00",
+  exercise_name: "osa999", course_id: "214", processed: true, 
+  all_tests_passed: true, points: "08-08.5345", processing_tried_at: "2017-09-20T10:29:01.437+03:00",
+  processing_began_at: "2017-07-20T10:29:02.754+03:00", processing_completed_at: "2017-07-20T10:29:17.364+03:00",
+  times_sent_to_sandbox: 1, processing_attempts_started_at: "2017-09-20T10:29:01.400+03:00", 
+  params_json: "{\"error_msg_locale\":\"fi\"}", requires_review: false, requests_review: false,
+  reviewed: false, message_for_reviewer: "", newer_submission_reviewed: false, review_dismissed: false,
+  paste_available: false, message_for_paste: "")
+
+Point.create(exercise_id: 2, point_id: 13, course_id: 1, user_id: 1, submission_id: 11, name: "08-11")
+
+Submission.create(submission_id: 12, user_id: 15653, created_at: "2017-08-05T10:29:01.400+03:00",
+  exercise_name: "osa999", course_id: "214", processed: true, 
+  all_tests_passed: true, points: "08-08.5345", processing_tried_at: "2017-09-20T10:29:01.437+03:00",
+  processing_began_at: "2017-07-20T10:29:02.754+03:00", processing_completed_at: "2017-07-20T10:29:17.364+03:00",
+  times_sent_to_sandbox: 1, processing_attempts_started_at: "2017-09-20T10:29:01.400+03:00", 
+  params_json: "{\"error_msg_locale\":\"fi\"}", requires_review: false, requests_review: false,
+  reviewed: false, message_for_reviewer: "", newer_submission_reviewed: false, review_dismissed: false,
+  paste_available: false, message_for_paste: "")
+
+Point.create(exercise_id: 2, point_id: 14, course_id: 1, user_id: 1, submission_id: 12, name: "08-12")
+
+Submission.create(submission_id: 13, user_id: 15653, created_at: "2017-08-08T10:29:01.400+03:00",
+  exercise_name: "osa999", course_id: "214", processed: true, 
+  all_tests_passed: true, points: "08-08.5345", processing_tried_at: "2017-09-20T10:29:01.437+03:00",
+  processing_began_at: "2017-07-20T10:29:02.754+03:00", processing_completed_at: "2017-07-20T10:29:17.364+03:00",
+  times_sent_to_sandbox: 1, processing_attempts_started_at: "2017-09-20T10:29:01.400+03:00", 
+  params_json: "{\"error_msg_locale\":\"fi\"}", requires_review: false, requests_review: false,
+  reviewed: false, message_for_reviewer: "", newer_submission_reviewed: false, review_dismissed: false,
+  paste_available: false, message_for_paste: "")
+
+Point.create(exercise_id: 2, point_id: 15, course_id: 1, user_id: 1, submission_id: 13, name: "08-13")
+
+Submission.create(submission_id: 14, user_id: 15653, created_at: "2017-08-10T10:29:01.400+03:00",
+  exercise_name: "osa999", course_id: "214", processed: true, 
+  all_tests_passed: true, points: "08-08.5345", processing_tried_at: "2017-09-20T10:29:01.437+03:00",
+  processing_began_at: "2017-07-20T10:29:02.754+03:00", processing_completed_at: "2017-07-20T10:29:17.364+03:00",
+  times_sent_to_sandbox: 1, processing_attempts_started_at: "2017-09-20T10:29:01.400+03:00", 
+  params_json: "{\"error_msg_locale\":\"fi\"}", requires_review: false, requests_review: false,
+  reviewed: false, message_for_reviewer: "", newer_submission_reviewed: false, review_dismissed: false,
+  paste_available: false, message_for_paste: "")
+
+Point.create(exercise_id: 2, point_id: 16, course_id: 1, user_id: 1, submission_id: 14, name: "08-14")
+
+Submission.create(submission_id: 15, user_id: 15653, created_at: "2017-08-11T10:29:01.400+03:00",
+  exercise_name: "osa999", course_id: "214", processed: true, 
+  all_tests_passed: true, points: "08-08.5345", processing_tried_at: "2017-09-20T10:29:01.437+03:00",
+  processing_began_at: "2017-07-20T10:29:02.754+03:00", processing_completed_at: "2017-07-20T10:29:17.364+03:00",
+  times_sent_to_sandbox: 1, processing_attempts_started_at: "2017-09-20T10:29:01.400+03:00", 
+  params_json: "{\"error_msg_locale\":\"fi\"}", requires_review: false, requests_review: false,
+  reviewed: false, message_for_reviewer: "", newer_submission_reviewed: false, review_dismissed: false,
+  paste_available: false, message_for_paste: "")
+
+Point.create(exercise_id: 2, point_id: 17, course_id: 1, user_id: 1, submission_id: 15, name: "08-15-1")
+
+Point.create(exercise_id: 2, point_id: 18, course_id: 1, user_id: 1, submission_id: 15, name: "08-15-2")
+
+Point.create(exercise_id: 2, point_id: 19, course_id: 1, user_id: 1, submission_id: 15, name: "08-15-3")
+
+Submission.create(submission_id: 16, user_id: 15653, created_at: "2017-08-12T10:29:01.400+03:00",
+  exercise_name: "osa999", course_id: "214", processed: true, 
+  all_tests_passed: true, points: "08-08.5345", processing_tried_at: "2017-09-20T10:29:01.437+03:00",
+  processing_began_at: "2017-07-20T10:29:02.754+03:00", processing_completed_at: "2017-07-20T10:29:17.364+03:00",
+  times_sent_to_sandbox: 1, processing_attempts_started_at: "2017-09-20T10:29:01.400+03:00", 
+  params_json: "{\"error_msg_locale\":\"fi\"}", requires_review: false, requests_review: false,
+  reviewed: false, message_for_reviewer: "", newer_submission_reviewed: false, review_dismissed: false,
+  paste_available: false, message_for_paste: "")
+
+Point.create(exercise_id: 2, point_id: 20, course_id: 1, user_id: 1, submission_id: 16, name: "08-16")
+
+Submission.create(submission_id: 17, user_id: 15653, created_at: "2017-08-13T10:29:01.400+03:00",
+  exercise_name: "osa999", course_id: "214", processed: true, 
+  all_tests_passed: true, points: "08-08.5345", processing_tried_at: "2017-09-20T10:29:01.437+03:00",
+  processing_began_at: "2017-07-20T10:29:02.754+03:00", processing_completed_at: "2017-07-20T10:29:17.364+03:00",
+  times_sent_to_sandbox: 1, processing_attempts_started_at: "2017-09-20T10:29:01.400+03:00", 
+  params_json: "{\"error_msg_locale\":\"fi\"}", requires_review: false, requests_review: false,
+  reviewed: false, message_for_reviewer: "", newer_submission_reviewed: false, review_dismissed: false,
+  paste_available: false, message_for_paste: "")
+
+Point.create(exercise_id: 2, point_id: 21, course_id: 1, user_id: 1, submission_id: 17, name: "08-17-1")
+
+Point.create(exercise_id: 2, point_id: 22, course_id: 1, user_id: 1, submission_id: 17, name: "08-17-2")
+
+Point.create(exercise_id: 2, point_id: 22, course_id: 1, user_id: 1, submission_id: 17, name: "08-17-3")
+
+Submission.create(submission_id: 18, user_id: 15653, created_at: "2017-08-13T10:29:01.400+03:00",
+  exercise_name: "osa999", course_id: "214", processed: true, 
+  all_tests_passed: true, points: "08-08.5345", processing_tried_at: "2017-09-20T10:29:01.437+03:00",
+  processing_began_at: "2017-07-20T10:29:02.754+03:00", processing_completed_at: "2017-07-20T10:29:17.364+03:00",
+  times_sent_to_sandbox: 1, processing_attempts_started_at: "2017-09-20T10:29:01.400+03:00", 
+  params_json: "{\"error_msg_locale\":\"fi\"}", requires_review: false, requests_review: false,
+  reviewed: false, message_for_reviewer: "", newer_submission_reviewed: false, review_dismissed: false,
+  paste_available: false, message_for_paste: "")
+
+Point.create(exercise_id: 2, point_id: 23, course_id: 1, user_id: 1, submission_id: 18, name: "08-18")
+
+Submission.create(submission_id: 19, user_id: 15653, created_at: "2017-08-15T10:29:01.400+03:00",
+  exercise_name: "osa999", course_id: "214", processed: true, 
+  all_tests_passed: true, points: "08-08.5345", processing_tried_at: "2017-09-20T10:29:01.437+03:00",
+  processing_began_at: "2017-07-20T10:29:02.754+03:00", processing_completed_at: "2017-07-20T10:29:17.364+03:00",
+  times_sent_to_sandbox: 1, processing_attempts_started_at: "2017-09-20T10:29:01.400+03:00", 
+  params_json: "{\"error_msg_locale\":\"fi\"}", requires_review: false, requests_review: false,
+  reviewed: false, message_for_reviewer: "", newer_submission_reviewed: false, review_dismissed: false,
+  paste_available: false, message_for_paste: "")
+
+Point.create(exercise_id: 2, point_id: 24, course_id: 1, user_id: 1, submission_id: 19, name: "08-19")
+
+Submission.create(submission_id: 20, user_id: 15653, created_at: "2017-08-16T10:29:01.400+03:00",
+  exercise_name: "osa999", course_id: "214", processed: true, 
+  all_tests_passed: true, points: "08-08.5345", processing_tried_at: "2017-09-20T10:29:01.437+03:00",
+  processing_began_at: "2017-07-20T10:29:02.754+03:00", processing_completed_at: "2017-07-20T10:29:17.364+03:00",
+  times_sent_to_sandbox: 1, processing_attempts_started_at: "2017-09-20T10:29:01.400+03:00", 
+  params_json: "{\"error_msg_locale\":\"fi\"}", requires_review: false, requests_review: false,
+  reviewed: false, message_for_reviewer: "", newer_submission_reviewed: false, review_dismissed: false,
+  paste_available: false, message_for_paste: "")
+
+Point.create(exercise_id: 2, point_id: 25, course_id: 1, user_id: 1, submission_id: 20, name: "08-20")
