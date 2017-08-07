13612b040a618b182f52394b1b0a6a23a2ad9195
Juuha <juha.ritakoski@hotmail.com>
2017-08-07 14:54:36 +0300

    Change endpoints to imitate TMC api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   resources :skills
   resources :exercises
 
-  get "/all-points", to: 'points#points_all'
+  get "/api/v8/courses/:course_id/points", to: 'points#points_all'
   get "/api/v8/courses/:course_id/exercises", to: 'exercises#index'
-  get "/api/v8/courses/:course_id/users/:user_id/skills-raw", to: 'skills#complexindex'
-  get "/skills-raw", to: 'skills#complex_index'
+  get "/api/v8/courses/:course_id/skills-raw", to: 'skills#complex_index'
   get "/api/v8/courses/:course_id/users/:user_id/skills", to: 'skills#index' 
-  get "single-points", to: 'points#index'
+  get "/api/v8/courses/:course_id/users/current/points", to: 'points#index'
   get "/api/v8/courses/:course_id/users/current/submissions", to: 'submissions#index'
 end
